6607675131a8d2786f30f4fe8409ea9040daf579
Avery Vine <averyvine@icloud.com>
2018-07-10 23:52:54 -0400

    some higher res images, more mobile friendly (index.html still needs work), general tweaks and qol improvements

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    if ($('html').scrollTop() < 50) {
-      $("html, body").animate({ scrollTop: 50 }, 500);
-    }
+    // if ($('html').scrollTop() < 50) {
+    //   $("html, body").animate({ scrollTop: 50 }, 500);
+    // }
+
+    $("a.external-link").attr('target' ,'_blank');
 });
