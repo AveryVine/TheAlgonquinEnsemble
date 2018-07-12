46ef0d043741229fb1162651dbeefebedbe15123
Avery Vine <averyvine@icloud.com>
2018-07-11 20:48:10 -0400

    mobile enhancements, font modifications, listen page, watch page, about us page enhancements

--- public/script.js
+++ public/script.js
@@ -3,5 +3,21 @@ $(document).ready(function () {
     //   $("html, body").animate({ scrollTop: 50 }, 500);
     // }
 
-    $("a.external-link").attr('target' ,'_blank');
+    setExternalLinksTarget();
+    setYouTubeVideoHeight();
+
+    window.addEventListener("resize", function() {
+        setYouTubeVideoHeight();
+    });
 });
+
+function setExternalLinksTarget() {
+    $("a.external-link").attr('target' ,'_blank');
+}
+
+function setYouTubeVideoHeight() {
+    let youtubeVideoRatio = 0.5625;
+    let youtubeWidth = $(".youtube").width();
+    let youtubeHeight = youtubeWidth * youtubeVideoRatio;
+    $(".youtube").css('height', youtubeHeight);
+}
