6a14c877a24d0b4dc843f0d3defc23b53c246cc9
Avery Vine <averyvine@icloud.com>
2018-07-18 23:15:56 -0400

    finished version 1

--- public/script.js
+++ public/script.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
     window.addEventListener("resize", function() {
         setYouTubeVideoHeight();
     });
+
+    setPhoneFooterClickAction();
 });
 
 function setExternalLinksTarget() {
@@ -20,4 +22,21 @@ function setYouTubeVideoHeight() {
     let youtubeWidth = $(".youtube").width();
     let youtubeHeight = youtubeWidth * youtubeVideoRatio;
     $(".youtube").css('height', youtubeHeight);
+}
+
+function setPhoneFooterClickAction() {
+    $("#footer-phone-text").click(function() {
+        var text1 = "Phone";
+        var text2 = "(613) 612-0623"
+        var html = $(this).html();
+        if (html == text1) {
+            $(this).html(text2);
+        } else {
+            $(this).html(text1);
+            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                window.location.href='tel:16136120623';
+            }
+        }
+        return false;
+    })
 }
