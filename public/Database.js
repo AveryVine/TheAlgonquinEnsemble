f0f45c6024780a7a5b8e10692ac25ada2234684c
Avery Vine <averyvine@icloud.com>
2018-11-29 20:40:06 -0500

    reverted home page event preview change, added new youtube video to watch page, added new allowed properties for all youtube videos

--- public/Database.js
+++ public/Database.js
@@ -12,7 +12,7 @@ class Database {
     }
 
     getUpcomingEvent() {
-        return this.getEvents(true, true)
+        return this.getEvents(true, false)
             .then(function (events) {
                 let event = events[0];
                 if (!event) {
@@ -27,30 +27,33 @@ class Database {
         return new Promise(function (resolve, reject) {
             collection.orderBy("date").get()
                 .then(function (querySnapshot) {
-                    let events = [];
+                    let upcomingEvents = [];
+                    let pastEvents = [];
                     let today = new Date();
                     querySnapshot.forEach(function (doc) {
                         let data = doc.data();
                         let eventDate = new Date(data.date.value);
-                        if (!CalendarEvent.isValidDate(eventDate) || (includeFuture && eventDate >= today) || (includePast && eventDate < today)) {
-                            let event = new CalendarEvent()
-                                .withTitle(data.title.value)
-                                .withSubtitle(data.subtitle.value)
-                                .withDescription(data.description.value)
-                                .withTicketPurchaseUrl((eventDate >= today) ? data.ticketPurchaseUrl.value : null)
-                                .withTime(data.time.value)
-                                .withDate(data.date.value)
-                                .withLocation(data.location.value)
-                                .withAddress(data.address.value)
-                                .withWebsite(data.website.value)
-                                .withPrice(data.price.value)
-                                .build();
-                            events.push(event)
+                        let event = new CalendarEvent()
+                            .withTitle(data.title.value)
+                            .withSubtitle(data.subtitle.value)
+                            .withDescription(data.description.value)
+                            .withTicketPurchaseUrl((eventDate >= today) ? data.ticketPurchaseUrl.value : null)
+                            .withTime(data.time.value)
+                            .withDate(data.date.value)
+                            .withLocation(data.location.value)
+                            .withAddress(data.address.value)
+                            .withWebsite(data.website.value)
+                            .withPrice(data.price.value)
+                            .build();
+                        if ((includeFuture && eventDate >= today) || !CalendarEvent.isValidDate(eventDate)) {
+                            upcomingEvents.push(event);
+                        }
+                        if (includePast && eventDate < today) {
+                            pastEvents.push(event);
                         }
                     });
-                    if (includePast && !includeFuture) {
-                        events = events.reverse()
-                    }
+                    pastEvents.reverse()
+                    let events = upcomingEvents.concat(pastEvents);
                     if (events.length === 0) {
                         events.push(CalendarEvent.getPlaceHolder());
                     }
